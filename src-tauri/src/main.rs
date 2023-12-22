a75a13da1e36b18c7d8fb46364a9581583e5132d
Annie Sexton <anniesexton@gmail.com>
2023-12-22 12:53:34 -0800

    wip

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,48 +1,109 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{fs, io};
-use serde::{Serialize};
+use std::{fs::{self, DirEntry}, io, path::{PathBuf, Path}, ffi::{OsString, OsStr}};
+use serde::{Serialize, Deserialize};
+use specta::Type;
+use tauri_specta::*;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn get_markdown() -> String {
-    let contents = fs::read_to_string("../data/test.md")
-        .expect("Should have been able to read the file");
-    contents.into()
+    let contents = fs::read_to_string("../data/test.md");
+        // .expect("Should have been able to read the file");
+    contents.unwrap()
 }
 
 #[tauri::command]
-fn save_markdown(text: String) -> bool {
+fn save_markdown(text: &str) -> bool {
     fs::write("../data/test.md", text).expect("Unable to write file");
     return true;
 }
 
 #[tauri::command]
-fn get_files_in_directory(root: &str) -> String {
-    // Get a list of all entries in the folder
-    println!("Running.....");
-    let result = fs::read_dir(root);
+#[specta::specta]
+fn get_directories(root: &str) -> Vec<String> {
+    let mut dir_content = Vec::new();
+    let paths = fs::read_dir(root).unwrap();
 
-    // Extract the filenames from the directory entries and store them in a vector
-    let file_names: Vec<String> = result.unwrap()
-        .filter_map(|entry| {
-            let path = entry.ok()?.path();
-            if path.is_file() {
-                path.file_name()?.to_str().map(|s| s.to_owned())
-            } else {
-                None
-            }
-        })
-        .collect();
-    return serde_json::to_string(&file_names).unwrap();
-    // println!("{json}");
-    // return file_names
+    for entry in paths {
+        let dir = entry.unwrap().path();
+        dbg!(&dir, dir.ends_with(".md"));
+        if dir.is_dir() {
+            println!("dir: {:?}", &dir);
+            dir_content.push(dir.file_name().unwrap().to_string_lossy().into_owned())
+        }
+        //  else if is_markdown_file(&dir) {
+        //     println!("file: {:?}", &dir);
+        //     dir_content.push(dir.file_name().unwrap().to_string_lossy().into_owned())
+        // }
+
+    };
+
+    dir_content
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, specta::Type)]
+pub struct Folder {
+    name: String,
+    children: Vec<Folder>
+}
+
+fn is_markdown_file(path: &Path) -> bool {
+
+    if !path.is_file() {return false}
+
+    if let Some(extension) = path.extension() {
+        if extension == "md" {return true}  
+    }
+
+    if path.to_str() == Some("README") {return true}
+
+    false
+}
+
+// #[derive(Serialize, Deserialize, Debug, Clone, specta::Type, tauri_specta::Event)]
+// pub struct DemoEvent(String);
+
+// #[derive(Serialize, Deserialize, Debug, Clone, specta::Type, tauri_specta::Event)]
+// pub struct EmptyEvent;
+
 fn main() {
+    let specta_builder = {
+        let specta_builder = ts::builder()
+            .commands(tauri_specta::collect_commands![
+                get_directories
+            ])
+            // .events(tauri_specta::collect_events![DemoEvent, EmptyEvent])
+            .config(specta::ts::ExportConfig::default().formatter(specta::ts::formatter::prettier));
+
+        #[cfg(debug_assertions)]
+        let specta_builder = specta_builder.path("../src/bindings.ts");
+
+        specta_builder.into_plugin()
+    };
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_markdown, save_markdown, get_files_in_directory])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    .plugin(specta_builder)
+    // .setup(|app| {
+    //     // let handle = app.handle();
+
+    //     // DemoEvent::listen_global(&handle, |event| {
+    //     //     dbg!(event.payload);
+    //     // });
+
+    //     // DemoEvent("Test".to_string()).emit_all(&handle).ok();
+
+    //     // EmptyEvent::listen_global(&handle, {
+    //     //     let handle = handle.clone();
+    //     //     move |_| {
+    //     //         EmptyEvent.emit_all(&handle).ok();
+    //     //     }
+    //     // });
+
+    //     Ok(())
+    // })
+    .invoke_handler(tauri::generate_handler![get_markdown, save_markdown, get_directories])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
